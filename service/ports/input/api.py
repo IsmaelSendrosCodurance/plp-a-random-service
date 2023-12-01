351719f35a721bbe97974fc1af706fad7f8b0f4f
IsmaelSendrosCodurance <128392676+IsmaelSendrosCodurance@users.noreply.github.com>
2023-12-01 20:23:47 +0100

    feat: make endpoint to return random responses

--- service/ports/input/api.py
+++ service/ports/input/api.py
@@ -1,13 +1,37 @@
-from fastapi import FastAPI
+import random
+from http import HTTPStatus
+
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI(
     title=f"PLP API",
     version="v1",
 )
 
+
 @app.get(
     path="/",
     summary="Healthcheck endpoint"
 )
 async def root():
-    return "I'm a random teapot!"
+    responses = [
+        {
+            'code': 200,
+            'message': 'I\'m a teapot !!'
+        },
+        {
+            'code': 404,
+            'message': 'Teapot not found :('
+        },
+        {
+            'code': 500,
+            'message': 'Teapot broken !!!!!!'
+        }
+    ]
+
+    response = random.choice(responses)
+
+    if response['code'] == '200':
+        return "I'm a random teapot!"
+    else:
+        raise HTTPException(status_code=HTTPStatus(response['code']),detail=response['message'])
