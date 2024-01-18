2c6b7f29806532cfac9887d67791d3f213691528
IsmaelSendrosCodurance <128392676+IsmaelSendrosCodurance@users.noreply.github.com>
2024-01-18 10:10:55 +0100

    fix: changes in terraform so ecr can pull images from registry

---
+++ main.tf
@@ -0,0 +1,133 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-3"
+}
+
+resource "aws_iam_role" "isendros-plp-ecs-service-role-tf" {
+  name                = "isendros-plp-ecs-service-role-tf"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "isendros-plp-iam-policy-attachment-tf" {
+  role = aws_iam_role.isendros-plp-ecs-service-role-tf.id
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy" "isendros-plp-iam-policy-tf" {
+  name = "isendros-plp-iam-policy-tf"
+  role = aws_iam_role.isendros-plp-ecs-service-role-tf.id
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "VisualEditor0",
+        "Effect": "Allow",
+        "Action": [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:GetAuthorizationToken"
+        ],
+        "Resource": "arn:aws:ecr:eu-west-3:300563897675:repository/isendros-plp"
+      }
+    ]
+  })
+}
+
+resource "aws_ecs_cluster" "isendros-plp-ecs-cluster-tf" {
+  name = "isendros-plp-ecs-cluster-tf"
+}
+
+resource "aws_ecs_task_definition" "isendros-plp-ecr-task-definition-tf" {
+  family = "isendros-plp-ecr-task-definition-tf"
+  task_role_arn = "${aws_iam_role.isendros-plp-ecs-service-role-tf.arn}"
+  execution_role_arn = "${aws_iam_role.isendros-plp-ecs-service-role-tf.arn}"
+  requires_compatibilities = ["FARGATE"]
+  network_mode = "awsvpc"
+  cpu       = 512
+  memory    = 1024
+  container_definitions = jsonencode([
+    {
+      name      = "isendros-plp-ecr-task-definition-tf"
+      image     = "300563897675.dkr.ecr.eu-west-3.amazonaws.com/isendros-plp:a-random-service-latest"
+      cpu       = 0
+      memory    = 1024
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+          appProtocol = "http"
+        }
+      ]
+      logConfiguration: {
+        logDriver: "awslogs",
+        options: {
+          awslogs-create-group: "true",
+          awslogs-group: "/ecs/isendros-plp-ecr-task-definition-tf",
+          awslogs-region: "eu-west-3",
+          awslogs-stream-prefix: "ecs"
+        },
+        secretOptions: []
+      }
+    }
+  ])
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture        = "X86_64"
+  }
+}
+
+variable "desired_count" {
+  type        = number
+}
+data "aws_subnet" "subnet_1" {
+  id ="subnet-075cbc4a"
+}
+data "aws_subnet" "subnet_2" {
+  id ="subnet-72b47f09"
+}
+data "aws_subnet" "subnet_3" {
+  id ="subnet-e060d189"
+}
+data "aws_security_group" "sg_1" {
+  id ="sg-951261fc"
+}
+data "aws_security_group" "sg_2" {
+  id ="sg-0b4207a15d05501c6"
+}
+
+
+resource "aws_ecs_service" "isendros-plp-ecs-service-tf" {
+  name            = "isendros-plp-ecs-service-tf"
+  cluster         = aws_ecs_cluster.isendros-plp-ecs-cluster-tf.name
+  task_definition = aws_ecs_task_definition.isendros-plp-ecr-task-definition-tf.arn
+  desired_count   = var.desired_count
+  depends_on      = [aws_iam_role_policy.isendros-plp-iam-policy-tf]
+  launch_type = "FARGATE"
+  network_configuration {
+    subnets = [data.aws_subnet.subnet_1.id,data.aws_subnet.subnet_2.id,data.aws_subnet.subnet_3.id]
+    security_groups = [data.aws_security_group.sg_1.id, data.aws_security_group.sg_2.id]
+    assign_public_ip = true
+  }
+
+}
